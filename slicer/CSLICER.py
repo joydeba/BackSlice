995657a1dfa6fb2829d2fb7df082e7614d755630
Debasish Chakroborti <joydeba@yahoo.com>
2023-08-04 02:45:32 -0600

    Module structure is defined.

---
+++ slicer/CSLICER.py
@@ -0,0 +1,14 @@
+
+class Cslicer():
+    def __init__(self, startCommit=None, endCommit=None, startDate = None, endDate = None):
+        self.startCommit = startCommit
+        self.endCommit = endCommit
+        self.startDate = startDate
+        self.endDate = endDate
+  
+    def analyzeProgram(self, sourceB= None):
+        slices = []
+        return slices
+    
+    def adapttoSCM(self, sourceB = None):
+        pass
---
+++ utils/slicerUtile.py
@@ -0,0 +1,36 @@
+def get_ast_diffs(sourceO, sourceB):
+    pass
+
+def get_hunk_dependencies(hunksB):
+    pass
+
+def get_changeset_dependencies(sourceB):
+    pass
+
+def get_changesets_and_metadata(sourceB):
+    pass
+
+def get_contexts(sourceB):
+    pass
+
+def get_functional_set(sourceB):
+    '''
+    The set of elements directly involved in a test case.
+    '''
+    pass
+
+def get_compilation_set(sourceB):
+    '''
+    Elements that are referenced by the functional set.
+    '''
+    pass
+def get_origin(fun_set=None, com_set= None):
+    commits = []
+    return commits
+
+def get_hunk_sets(commit=None):
+    dependencies = []
+    return dependencies
+
+def get_stable_version_libraries(sVersion, sourceB):
+    pass
