d691e56c9636d42e579a04dacedace14e1139c23
Debasish Chakroborti <joydeba@yahoo.com>
2023-08-16 12:47:30 -0600

    Doc strings added for function definition.

--- slicer/CSLICER.py
+++ slicer/CSLICER.py
@@ -1,3 +1,4 @@
+from utils.slicerUtile import *
 
 class Cslicer():
     def __init__(self, startCommit=None, endCommit=None, startDate = None, endDate = None):
@@ -6,9 +7,38 @@ class Cslicer():
         self.startDate = startDate
         self.endDate = endDate
   
-    def analyzeProgram(self, sourceB= None):
+    def analyzeProgram(self, sourceBbase= None, originalHistory = None, testCases = None):
         slices = []
+        '''
+        - To preserve the "behavior" of the functional and compilation set elements, 
+        the changes needed to be collected in reverse order, starting from the newest 
+        change set ΔN and going back to the oldest one ΔO. This process will ensure 
+        that the elements' behavior remains consistent throughout the changes.
+
+        - The decision regarding whether to keep or remove entities is based on the 
+        changes that occurred and their respective change types. Entities will be 
+        retained if they belong to the functional or compilation set; otherwise, 
+        they will be removed.
+
+
+        - The sliced history incorporates an atomic change δ if it meets specific criteria. 
+        These criteria include being an insertion or update to the entities within the functional
+        set or an insertion to the entities in the compilation set. However, updates to the compilation
+        set entities are disregarded as they typically do not impact the test results significantly.
+
+        '''
+
+        # Set (Λ) includes all fields explicitly initialized during declaration and all
+        #  methods (and constructors) called during runtime.
+
+        functinalSet = get_functional_set()
+        compilationSet = get_compilation_set()
+
+        # The procedure COMPDEP analyzes refer- ence relations in pk and includes 
+        # all referenced code entities of Λ into the compilation set Π.
+        # set of rules for computing Π from [10]
+
+
         return slices
     
-    def adapttoSCM(self, sourceB = None):
-        pass
+
--- utils/slicerUtile.py
+++ utils/slicerUtile.py
@@ -16,20 +16,40 @@ def get_contexts(sourceB):
 def get_functional_set(sourceB):
     '''
     The set of elements directly involved in a test case.
+
+    During the execution of a test, the set of source code entities 
+    (such as methods or classes) that include the statements being 
+    traversed is referred to as the functional set, symbolized as Λ. 
+    In the present example, the functional set comprises {Boo.boo2, Bar.bar1}.
     '''
     pass
 
 def get_compilation_set(sourceB):
     '''
     Elements that are referenced by the functional set.
+
+    Following the test execution, all code entities referenced within the functional set are 
+    ensured to be defined, regardless of whether they were traversed during the tests. 
+    The entities included in the compilation set are {Boo, Boo.b, Bar}.
     '''
     pass
+
+def adapttoSCM(self, sourceB = None):
+    '''
+    - mapped back to the original commits/reverting unwanted changes
+    - hunk dependencies
+
+    '''
+    dependencies = get_hunk_sets()
+    pass
+
 def get_origin(fun_set=None, com_set= None):
     commits = []
     return commits
 
 def get_hunk_sets(commit=None):
     dependencies = []
+    # Gether context 
     return dependencies
 
 def get_stable_version_libraries(sVersion, sourceB):
