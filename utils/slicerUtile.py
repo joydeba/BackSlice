ff3e21a8e746e3cab268b10fb73eb6f73774c237
Debasish Chakroborti <joydeba@yahoo.com>
2023-08-17 23:21:50 -0600

    Def added for functional set.

--- utils/slicerUtile.py
+++ utils/slicerUtile.py
@@ -47,28 +47,220 @@ def get_ast_diffs(source_commits):
 #         print(line)
 #     print()
 
-def get_hunk_dependencies(sourceB):
-    pass
+def get_hunk_context(file_content, hunk_start, hunk_end, context_lines=3):
+    lines = file_content.split('\n')
+    hunk_range = range(hunk_start, hunk_end)
+    context = []
 
-def get_changeset_dependencies(sourceB):
-    pass
+    for lineno in hunk_range:
+        if lineno < 1 or lineno > len(lines):
+            continue
 
-def get_changesets_and_metadata(sourceB):
-    pass
+        line = lines[lineno - 1]
+        context.append(line)
 
-def get_contexts(sourceB):
-    pass
+    # Add surrounding lines as context
+    for i in range(context_lines):
+        before = hunk_start - i - 2
+        after = hunk_end + i - 1
 
-def get_functional_set(sourceB):
+        if before >= 0:
+            context.insert(0, lines[before])
+
+        if after < len(lines):
+            context.append(lines[after])
+
+    return '\n'.join(context)
+
+# # Example usage
+# file_content = """
+# def foo():
+#     print("Hello")
+    
+# def bar():
+#     print("World")
+# """
+
+# hunk_start = 2
+# hunk_end = 4
+
+# context = get_hunk_context(file_content, hunk_start, hunk_end)
+# print(context)
+
+def get_changeset_dependencies(source_code):
+    dependencies = set()
+
+    def visit_Import(node):
+        for alias in node.names:
+            dependencies.add(alias.name)
+
+    def visit_ImportFrom(node):
+        module_name = node.module
+        if module_name is not None:
+            dependencies.add(module_name)
+    
+    tree = ast.parse(source_code)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            visit_Import(node)
+        elif isinstance(node, ast.ImportFrom):
+            visit_ImportFrom(node)
+    
+    return list(dependencies)
+
+# # Example usage:
+# source_code = """
+# import os
+# from math import sqrt
+# import numpy as np
+# from datetime import datetime
+# """
+# dependencies = get_changeset_dependencies(source_code)
+# print(dependencies)
+
+def get_changesets_and_metadata(pull_request, sourceB):
+    """
+    Extracts pull request title, body, tags, and comments from submitted source code.
+
+    Args:
+        pull_request (dict): Dictionary containing pull request details.
+        sourceB (str): Source code submitted in the pull request.
+
+    Returns:
+        tuple: A tuple containing pull request title (str), body (str), tags (list of str), and comments (list of str).
+    """
+    title = pull_request.get('title', '')
+    body = pull_request.get('body', '')
+    tags = pull_request.get('tags', [])
+    
+    # Extract comments from the source code
+    comments = []
+    lines = sourceB.split('\n')
+    is_comment_block = False
+    comment_block = []
+
+    for line in lines:
+        stripped_line = line.strip()
+        
+        if stripped_line.startswith('#') or stripped_line.startswith('//'):
+            # Single-line comment
+            comments.append(stripped_line.lstrip('#').lstrip('//').strip())
+        elif stripped_line.startswith('"""') or stripped_line.startswith("'''"):
+            # Multi-line comment start
+            is_comment_block = True
+            comment_block.append(stripped_line.lstrip('"""').lstrip("'''").strip())
+        elif is_comment_block and (stripped_line.endswith('"""') or stripped_line.endswith("'''")):
+            # Multi-line comment end
+            is_comment_block = False
+            comment_block.append(stripped_line.rstrip('"""').rstrip("'''").strip())
+            comments.append('\n'.join(comment_block))
+            comment_block = []
+        elif is_comment_block:
+            # Inside multi-line comment
+            comment_block.append(stripped_line)
+
+    return title, body, tags, comments
+
+# # Example usage
+# pull_request_data = {
+#     'title': 'Add new feature',
+#     'body': 'This pull request adds a new feature to the project.',
+#     'tags': ['feature', 'enhancement'],
+# }
+
+# source_code = """
+# # This is a single-line comment.
+# def add(a, b):
+#     """
+#     This is a multi-line comment.
+#     It explains the function purpose and usage.
+#     """
+#     return a + b
+# """
+
+# title, body, tags, comments = get_commentDoc_and_metadata(pull_request_data, source_code)
+
+# print("Title:", title)
+# print("Body:", body)
+# print("Tags:", tags)
+# print("Comments:", comments)
+
+def find_source_code_entity(source_code, target_line):
+    # Parse the source code
+    parsed_tree = ast.parse(source_code)
+
+    # Define a visitor to traverse the parsed tree
+    class EntityVisitor(ast.NodeVisitor):
+        def __init__(self, target_line):
+            self.target_line = target_line
+            self.found_entity = None
+
+        def visit_FunctionDef(self, node):
+            # Check if the target line is within the function's range
+            if node.lineno <= self.target_line <= node.end_lineno:
+                self.found_entity = node.name
+
+        def visit_ClassDef(self, node):
+            # Check if the target line is within the class's range
+            if node.lineno <= self.target_line <= node.end_lineno:
+                self.found_entity = node.name
+
+    # Create an instance of the EntityVisitor and visit the parsed tree
+    visitor = EntityVisitor(target_line)
+    visitor.visit(parsed_tree)
+
+    return visitor.found_entity
+
+def get_functional_set(sourceCode, testCases):
     '''
-    The set of elements directly involved in a test case.
+    The set of elements of the sourceCode directly involved in a test case.
 
-    During the execution of a test, the set of source code entities 
-    (such as methods or classes) that include the statements being 
-    traversed is referred to as the functional set, symbolized as Λ. 
-    In the present example, the functional set comprises {Boo.boo2, Bar.bar1}.
+    During the execution of testCases, the set of source code entities
+    (such as methods or classes) of sourceCode that include the statements being
+    traversed by testCases is referred to as the functional set, symbolized as Λ.
+
+    Args:
+        sourceCode (str): The source code to analyze.
+        testCases (list): List of test cases.
+
+    Returns:
+        set: The functional set of source code entities involved in the test cases.
     '''
-    pass
+
+    '''
+    This code uses the ast module to traverse the abstract syntax tree of the Python source code. The EntityVisitor class defines methods to visit function and class definitions and checks if the target line from the test cases falls within their range. If a match is found, the entity name is stored in the found_entity attribute. The get_functional_set function then calls find_source_code_entity for each test case line and builds the functional set based on the found entities.
+    '''
+
+    functional_set = set()
+
+    for testCase in testCases:
+        for line in testCase:
+            entity = find_source_code_entity(sourceCode, line)
+            if entity:
+                functional_set.add(entity)
+
+    return functional_set
+
+# # Example usage
+# source_code = '''
+# class MathUtils:
+#     def add(self, a, b):
+#         return a + b
+
+# def square(x):
+#     return x * x
+
+# result = MathUtils().add(3, 5)
+# squared_result = square(result)
+# '''
+
+# test_cases = [
+#     ['result = MathUtils().add(3, 5)'],
+#     ['squared_result = square(result)']
+# ]
+
+# functional_set = get_functional_set(source_code, test_cases)
+# print(functional_set)
 
 def get_compilation_set(sourceB):
     '''
