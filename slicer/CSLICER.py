87ddecbab2d84b4f981e38923a5622532bc205c5
Debasish Chakroborti <joydeba@yahoo.com>
2023-10-27 19:25:33 -0600

    complets analyzeProgram def for CSLICER.

--- slicer/CSLICER.py
+++ slicer/CSLICER.py
@@ -15,83 +15,50 @@ class Cslicer():
 
 
     def analyzeProgram(self):
+
+        """
+        Adapt the sourceOriginal to a stable version based on various inputs.
+
+        Parameters:
+            astdiffsHistory (list): A list of tuples where each tuple contains (commit_a, commit_b, list(diff)).
+            context (str): The context code of the sourceOriginal in string form.
+            dependencies (list): A list of new dependency changes for sourceOriginal.
+            metadata (tuple): Metadata of a pull request, including (title, body, tags, comments).
+            functionalSet (set): A set of function names covered by test cases.
+            compilationSet (set): A set of functions in sourceOriginal referenced by functionalSet.
+            stableLibraris (dict): Library information in the form of {dir_item['name']: {
+                'libraries': list(set(libraries)),
+                'function_names': list(set(function_names)),
+                'function_calls': list(set(function_calls)),
+                'class_names': list(set(class_names)),
+                'class_method_calls': list(set(class_method_calls))
+            }}.
+            targetfile (str): A string representation of the target source file.
+
+        Returns:
+            str: The adapted source code, close to sourcebackport.
+
+        """        
         adaptedSource = self.sourceOriginal  # Initialize with the original source
 
-        if self.targetfile and self.functionalSet and self.compilationSet:
-            # Check if targetfile contains statements related to functionalSet or compilationSet
-            target_statements = self.targetfile.split("\n")  # Read the contents of targetfile
+        if self.astdiffsHistory and self.functionalSet and self.compilationSet:
+            # Iterate over astdiffsHistory to check for statements related to functionalSet or compilationSet
+            for commit_a, commit_b, diff in self.astdiffsHistory:
+                for change in diff:
+                    if any(statement in change for statement in self.functionalSet):
+                        if change not in adaptedSource:
+                            # If a statement from functionalSet is not already in adaptedSource, add it
+                            adaptedSource += f"\n{change}"
+                    if any(statement in change for statement in self.compilationSet):
+                        if change not in adaptedSource:
+                            # If a statement from compilationSet is not already in adaptedSource, add it
+                            adaptedSource += f"\n{change}"
 
-            # Iterate over the functionalSet and add statements to adaptedSource
-            for statement in self.functionalSet:
-                for t_statement in target_statements:
-                    if statement in t_statement:
-                        adaptedSource += f"\n{t_statement}"
-
-            # Iterate over the compilationSet and add statements to adaptedSource
-            for statement in self.compilationSet:
-                for t_statement in target_statements:
-                    if statement in t_statement:
-                        adaptedSource += f"\n{t_statement}"
-
+        # Add the adaptedSource in the middle of the context for further adaptation
+        adaptedSource = self.context.replace('{{INSERT_HERE}}', adaptedSource)
+        
         return adaptedSource
 
-    # def analyzeProgram(self):
-        
-    #     adaptedSource = None
-    #     # Todo- with the help of astdiffsHistory, context, dependencies, metadata, functionalSet, compilationSet, stableLibraris - adapt sourceOriginal for stable version so that it can be close to sourcebackport
-    #     # The form of the astdiffsHistory is (commit_a, commit_b, list(diff))
-    #     # Context gives the sourceOriginal context code in string form 
-    #     # Dependencies is list to give any new dependency chage for sourceOriginal
-    #     # FunctionalSet is a set for for the function name covered by the test cases 
-    #     # CompilationSet is a set for the function of sourceOriginal referenced by functionalSet 
-    #     # Metadat is the - (title, body, tags, comments) of a pull requests 
-    #     # StableLibraris is the library information in the form of - {dir_item['name']: {
-    #     #     'libraries': list(set(libraries)),
-    #     #     'function_names': list(set(function_names)),
-    #     #     'function_calls': list(set(function_calls)),
-    #     #     'class_names': list(set(class_names)),
-    #     #     'class_method_calls': list(set(class_method_calls))
-    #     # }}
-    #     # targetfile is the string representation of the target source file 
 
-    #     # The ligic should be something like this 
-    #     # - targetFile has a staement related to functionalSet add it to sourceOriginal to adapt
-    #     # - targetFile has a staement related to compilationSet add it to sourceOriginal to adapt
-
-    #     return adaptedSource        
-  
-    # def analyzeProgram(self):
-    #     slices = []
-    #     '''
-    #     - To preserve the "behavior" of the functional and compilation set elements, 
-    #     the changes needed to be collected in reverse order, starting from the newest 
-    #     change set ΔN and going back to the oldest one ΔO. This process will ensure 
-    #     that the elements' behavior remains consistent throughout the changes.
-
-    #     - The decision regarding whether to keep or remove entities is based on the 
-    #     changes that occurred and their respective change types. Entities will be 
-    #     retained if they belong to the functional or compilation set; otherwise, 
-    #     they will be removed.
-
-
-    #     - The sliced history incorporates an atomic change δ if it meets specific criteria. 
-    #     These criteria include being an insertion or update to the entities within the functional
-    #     set or an insertion to the entities in the compilation set. However, updates to the compilation
-    #     set entities are disregarded as they typically do not impact the test results significantly.
-
-    #     '''
-
-    #     # Set (Λ) includes all fields explicitly initialized during declaration and all
-    #     #  methods (and constructors) called during runtime.
-
-    #     functinalSet = self.functionalSet
-    #     compilationSet = self.compilationSet
-
-    #     # The procedure COMPDEP analyzes refer- ence relations in pk and includes 
-    #     # all referenced code entities of Λ into the compilation set Π.
-    #     # set of rules for computing Π from [10]
-
-
-    #     return slices
     
 
