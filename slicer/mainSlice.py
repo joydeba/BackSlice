d86d503c2e471860a4337ddfef293654226718e6
Debasish Chakroborti <joydeba@yahoo.com>
2024-02-26 21:36:34 -0600

    Missmatch issue resolved

--- slicer/mainSlice.py
+++ slicer/mainSlice.py
@@ -142,8 +142,8 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
 
                             for indexHunks0 in range(1, min_hunks_count):
 
-                                commits_hunkline_original_context = commits_diffs_original_contextHunks[original_hunks_count - indexHunks0].split("\n")
-                                commits_hunkline_backport_context = commits_diffs_backport_contextHunks[backport_hunks_count - indexHunks0].split("\n")
+                                commits_hunkline_original_context = commits_diffs_original_contextHunks[min_hunks_count - indexHunks0].split("\n")
+                                commits_hunkline_backport_context = commits_diffs_backport_contextHunks[min_hunks_count - indexHunks0].split("\n")
                                 hunkStartLnNo = commits_hunkline_original_context[0].split(" ")[0][1:].split(",")
                                 hunkEndlnNo = commits_hunkline_original_context[0].split(" ")[1][1:].split(",")
 
