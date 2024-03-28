27661970d85b96f478246a0eb0630259df27483a
Joy'sMaclab <joydeba@yahoo.com>
2024-03-28 11:00:18 -0600

    Out structured.

--- slicer/mainSlice.py
+++ slicer/mainSlice.py
@@ -268,17 +268,7 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
 
                             functionalSetforHunk = get_functional_set(codeHunk, testCases = testhunks_original)
                             astdiffshistory = get_ast_diffs(source_commits = pull_commitsSubmitted, startCommit=None, endCommit=None, startDate = None, endDate = None, repoName=repoName, projectName =projectName) 
-                            cslicer = Cslicer(sourceOriginal = codeHunk,
-                                                sourcebackport = codeHunkBackport, 
-                                                astdiffsHistory = astdiffshistory, 
-                                                context = get_hunk_context(file_content = codehunks_original_withContext[context_index], hunk_start = hunkStartLnNo, hunk_end = hunkEndlnNo, context_lines=3), 
-                                                dependencies = get_changeset_dependencies(previousBackportfullFileTarget), 
-                                                metadata = get_changesets_and_metadata(pull_request = pull_backport, sourceO = codeHunkBackport), 
-                                                functionalSet = functionalSetforHunk, 
-                                                compilationSet= get_compilation_set(sourceCode = codeHunk, functional_set = functionalSetforHunk), 
-                                                stableLibraris = get_stable_version_libraries(owner = repoName, repo = projectName, branch = targetStableBranch, github_token=ghkey, cache_file= projectName+"StableLibraryCsche"), 
-                                                targetfile = previousBackportfullFileTarget)
-                            # cslicer = BackSlicer(sourceOriginal = codeHunk,
+                            # cslicer = Cslicer(sourceOriginal = codeHunk,
                             #                     sourcebackport = codeHunkBackport, 
                             #                     astdiffsHistory = astdiffshistory, 
                             #                     context = get_hunk_context(file_content = codehunks_original_withContext[context_index], hunk_start = hunkStartLnNo, hunk_end = hunkEndlnNo, context_lines=3), 
@@ -287,7 +277,17 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
                             #                     functionalSet = functionalSetforHunk, 
                             #                     compilationSet= get_compilation_set(sourceCode = codeHunk, functional_set = functionalSetforHunk), 
                             #                     stableLibraris = get_stable_version_libraries(owner = repoName, repo = projectName, branch = targetStableBranch, github_token=ghkey, cache_file= projectName+"StableLibraryCsche"), 
-                            #                     targetfile = previousBackportfullFileTarget)                        
+                            #                     targetfile = previousBackportfullFileTarget)
+                            cslicer = BackSlicer(sourceOriginal = codeHunk,
+                                                sourcebackport = codeHunkBackport, 
+                                                astdiffsHistory = astdiffshistory, 
+                                                context = get_hunk_context(file_content = codehunks_original_withContext[context_index], hunk_start = hunkStartLnNo, hunk_end = hunkEndlnNo, context_lines=3), 
+                                                dependencies = get_changeset_dependencies(previousBackportfullFileTarget), 
+                                                metadata = get_changesets_and_metadata(pull_request = pull_backport, sourceO = codeHunkBackport), 
+                                                functionalSet = functionalSetforHunk, 
+                                                compilationSet= get_compilation_set(sourceCode = codeHunk, functional_set = functionalSetforHunk), 
+                                                stableLibraris = get_stable_version_libraries(owner = repoName, repo = projectName, branch = targetStableBranch, github_token=ghkey, cache_file= projectName+"StableLibraryCsche"), 
+                                                targetfile = previousBackportfullFileTarget)                        
                             context_index = context_index +1                    
                             slicebyCslicer, recommendation = cslicer.analyzeProgram()
                             recommendation = recommendation + "\nPRs: "+ pull_id_original  + ", "  + pull_id_backport
@@ -307,7 +307,7 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
                     continue
         
 
-    with open(projectName+"InconICFDiffzBackSlice.txt", 'w') as f:   
+    with open("slicerOutput/"+projectName+"InconICFDiffzBackSliceCPython.txt", 'w') as f:   
         print("Total Labeled Backporting PRs", len(data_read), file=f)
         print("Total Sliced Required", numberofSlicingRequired, file=f)
         print("Total Number of Hunk Differences", numberofDifferences, file=f)
@@ -377,23 +377,23 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
     
 
 # # Updated on 11th April 2023
-ansibleDictOfActiveBranches = {'devel':{}, 'stable-2.9':{}, 'stable-2.12':{}, 'stable-2.14':{}, 'stable-2.13':{}, 'stable-2.15':{}}
+# ansibleDictOfActiveBranches = {'devel':{}, 'stable-2.9':{}, 'stable-2.12':{}, 'stable-2.14':{}, 'stable-2.13':{}, 'stable-2.15':{}}
 # bitcoinDictOfActiveBranches = {'master':{}, '25.x':{}, '24.x':{}, '23.x':{}, '22.x':{}}
 # ElasticsearchDictOfActiveBranches = {'main':{}, '8.8':{}, '7.17':{}, '8.7':{}, '8.5':{}, '8.6':{}, '8.0':{}, '8.1':{}, '8.2':{}, '8.3':{},'8.4':{}, '6.5':{}}
 # JuliaDictOfActiveBranches = {'master':{}, 'release-1.9':{}, 'release-1.8':{}, 'release-1.6':{}}
 # RailsDictOfActiveBranches = {'main':{}, '7-0-stable':{}, '6-1-stable':{}, '6-0-stable':{}}
 # KibanaDictOfActiveBranches = {'main':{}, '8.8':{}, '8.7':{}, '8.6':{}, '8.5':{}, '8.4':{}, '7.17':{}, '8.2':{}, '8.3':{}}
-# cpythonDictOfActiveBranches = {'main':{}, '3.12':{}, '3.11':{}, '3.10':{}, '3.9':{}, '3.8':{}}
-ansibleDefault_branch = 'devel'
+cpythonDictOfActiveBranches = {'main':{}, '3.12':{}, '3.11':{}, '3.10':{}, '3.9':{}, '3.8':{}}
+# ansibleDefault_branch = 'devel'
 # bitcoinDefault_branch = 'master'
 # ElasticsearchDefault_branch = 'main'
 # JuliaDefault_branch = 'master'
 # RailsDefault_branch = 'main'
 # KibanaDefault_branch = 'main'
-# cpythonDefault_branch = 'main'
-mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Ansible_backport_keywordsPRs.csv', 
-ansibleDefault_branch,
-ansibleDictOfActiveBranches,
-'ansible',
-'ansible',
-'data_cmp_incmpWithTest/Incmp_BackSlice_Ansible_backport_keywordsPRs.csv')
+cpythonDefault_branch = 'main'
+mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Cpython_backport_keywordsPRs.csv', 
+cpythonDefault_branch,
+cpythonDictOfActiveBranches,
+'python',
+'cpython',
+'slicerOutput/Incmp_BackSlice_Cpython_backport_keywordsPRs.csv')
