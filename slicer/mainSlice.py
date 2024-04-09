c866310ddbc8b0a6bd8c7959541282396435db3f
Debasish Chakroborti <joydeba@yahoo.com>
2024-04-09 07:48:12 -0600

    Running for Salt

--- slicer/mainSlice.py
+++ slicer/mainSlice.py
@@ -320,7 +320,7 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
                     print(e)
                     continue
         
-    with open("slicerOutput/"+projectName+"InconICFDiffzBackSliceNoNeedTestSample.txt", 'w') as f:   
+    with open("slicerOutput/"+projectName+"InconICFDiffzBackSliceNoNeedTest.txt", 'w') as f:   
         print("Total Labeled Backporting PRs", len(data_read), file=f)
         print("Total Sliced Required", numberofSlicingRequired, file=f)
         print("Total Number of Hunk Differences", numberofDifferences, file=f)
@@ -387,7 +387,7 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
 
 # # Updated on 11th April 2023
 # ansibleDictOfActiveBranches = {'devel':{}, 'stable-2.9':{}, 'stable-2.12':{}, 'stable-2.14':{}, 'stable-2.13':{}, 'stable-2.15':{}, 'stable-2.16':{}}
-ansibleDictOfActiveBranches = {"stable-2.3": 2, "stable-2.4": 1, "stable-2.5": 2, "stable-2.6": 49, "stable-2.7": 458, "stable-2.8": 603, "stable-2.9": 706, "stable-2.10": 294, "stable-2.11": 0, "temporary-2.9.1-branch-releng-only": 27}
+# ansibleDictOfActiveBranches = {"stable-2.3": 2, "stable-2.4": 1, "stable-2.5": 2, "stable-2.6": 49, "stable-2.7": 458, "stable-2.8": 603, "stable-2.9": 706, "stable-2.10": 294, "stable-2.11": 0, "temporary-2.9.1-branch-releng-only": 27}
 # bitcoinDictOfActiveBranches = {'master':{}, '25.x':{}, '24.x':{}, '23.x':{}, '22.x':{}, '27.x':{}, '26.x':{}}
 # bitcoinDictOfActiveBranches = { '0.19': {}, '0.20': {}, '0.21': {}, '0.18': {}, '0.17': {}, '0.16': {}, '0.15': {}, '0.14': {}, '0.13': {}, '0.12': {}, '0.11': {}, '0.10': {},'0.9.3': {}}
 # ElasticsearchDictOfActiveBranches = {'main':{}, '8.8':{}, '7.17':{}, '8.7':{}, '8.5':{}, '8.6':{}, '8.0':{}, '8.1':{}, '8.2':{}, '8.3':{},'8.4':{}, '6.5':{}}
@@ -396,8 +396,8 @@ ansibleDictOfActiveBranches = {"stable-2.3": 2, "stable-2.4": 1, "stable-2.5": 2
 # KibanaDictOfActiveBranches = {'main':{}, '8.8':{}, '8.7':{}, '8.6':{}, '8.5':{}, '8.4':{}, '7.17':{}, '8.2':{}, '8.3':{}}
 # cpythonDictOfActiveBranches = {'main':{}, '3.12':{}, '3.11':{}, '3.10':{}, '3.9':{}, '3.8':{}, '3.0.x':{}}
 # cmsswDictOfActiveBranches = {'master':{}, 'CMSSW_14_1_DEVEL_X':{}, 'CMSSW_14_1_X':{}, 'CMSSW_14_0_X':{}, 'CMSSW_13_0_HeavyIon_X':{}, 'CMSSW_10_6_X':{}, 'CMSSW_13_2_X':{}, 'CMSSW_13_3_X':{}, 'CMSSW_12_4_X':{}, 'CMSSW_13_0_X':{}, 'CMSSW_12_6_X':{}, 'CMSSW_13_1_X':{}, 'CMSSW_14_0_DEVEL_X':{}, 'CMSSW_7_1_X':{}, 'CMSSW_12_5_X':{}}
-# saltDictOfActiveBranches = {'master':{}, '3006.x':{}, '3007.x':{}, '3005.x':{}, '2018.3':{}, '2019.2':{}, 'freeze':{} }
-ansibleDefault_branch = 'devel' # Python 87.8% ---------
+saltDictOfActiveBranches = {'master':{}, '3006.x':{}, '3007.x':{}, '3005.x':{}, '2018.3':{}, '2019.2':{}, 'freeze':{} }
+# ansibleDefault_branch = 'devel' # Python 87.8% ---------
 # bitcoinDefault_branch = 'master' # Python 20.1% ---------
 # ElasticsearchDefault_branch = 'main' # Python 0.0%
 # JuliaDefault_branch = 'master' # Python 0.0%
@@ -411,7 +411,7 @@ ansibleDefault_branch = 'devel' # Python 87.8% ---------
 # nextcloudDefault = "maser" # Python 0.0%
 # nixpkgsDefault = "master" #  Python 1.2%
 # owncloudDefault  = "maser" # Python 0.0%
-# saltDefault  = "maser" # Python 97.8% --------- X
+saltDefault_branch  = "maser" # Python 97.8% --------- X
 
 
 # mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Cmssw_backport_keywordsPRs.csv', 
@@ -421,11 +421,11 @@ ansibleDefault_branch = 'devel' # Python 87.8% ---------
 # 'cmssw',
 # 'slicerOutput/Incmp_BackSlice_Cmssw_backport_keywordsPRs.csv')
 
-mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Ansible_backport_keywordsPRsNoTestNeededSample.csv', 
-ansibleDefault_branch,
-ansibleDictOfActiveBranches,
-'ansible',
-'ansible',
-'slicerOutput/Incmp_BackSlice_Ansible_backport_keywordsPRsNoNeedTestSample.csv',
-'stable-2.17'
+mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Salt_backport_keywordsPRsNoTestNeeded.csv', 
+saltDefault_branch,
+saltDictOfActiveBranches,
+'saltstack',
+'salt',
+'slicerOutput/Incmp_BackSlice_Salt_backport_keywordsPRsNoNeedTestSample.csv',
+'3006.x'
 )
