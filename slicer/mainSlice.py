17824ef8ff9055ffc50d4f727895b01b41dc849d
Joy'sMaclab <joydeba@yahoo.com>
2024-08-08 15:20:44 -0600

    CPython and Salt Data

--- slicer/mainSlice.py
+++ slicer/mainSlice.py
@@ -386,16 +386,16 @@ def mainCSLICER(prlist = 'prlist.csv', default_branch='main', dictOfActiveBranch
 # JuliaDictOfActiveBranches = {'master':{}, 'release-1.9':{}, 'release-1.8':{}, 'release-1.6':{}}
 # RailsDictOfActiveBranches = {'main':{}, '7-0-stable':{}, '6-1-stable':{}, '6-0-stable':{}}
 # KibanaDictOfActiveBranches = {'main':{}, '8.8':{}, '8.7':{}, '8.6':{}, '8.5':{}, '8.4':{}, '7.17':{}, '8.2':{}, '8.3':{}}
-cpythonDictOfActiveBranches = {'main':{}, '3.12':{}, '3.11':{}, '3.10':{}, '3.9':{}, '3.8':{}, '3.0.x':{}}
+# cpythonDictOfActiveBranches = {'main':{}, '3.12':{}, '3.11':{}, '3.10':{}, '3.9':{}, '3.8':{}, '3.0.x':{}}
 # cmsswDictOfActiveBranches = {'master':{}, 'CMSSW_14_1_DEVEL_X':{}, 'CMSSW_14_1_X':{}, 'CMSSW_14_0_X':{}, 'CMSSW_13_0_HeavyIon_X':{}, 'CMSSW_10_6_X':{}, 'CMSSW_13_2_X':{}, 'CMSSW_13_3_X':{}, 'CMSSW_12_4_X':{}, 'CMSSW_13_0_X':{}, 'CMSSW_12_6_X':{}, 'CMSSW_13_1_X':{}, 'CMSSW_14_0_DEVEL_X':{}, 'CMSSW_7_1_X':{}, 'CMSSW_12_5_X':{}}
-# saltDictOfActiveBranches = {'master':{}, '3006.x':{}, '3007.x':{}, '3005.x':{}, '2018.3':{}, '2019.2':{}, 'freeze':{} }
+saltDictOfActiveBranches = {'master':{}, '3006.x':{}, '3007.x':{}, '3005.x':{}, '2018.3':{}, '2019.2':{}, 'freeze':{} }
 # ansibleDefault_branch = 'devel' # Python 87.8% ---------
 # bitcoinDefault_branch = 'master' # Python 20.1% ---------
 # ElasticsearchDefault_branch = 'main' # Python 0.0%
 # JuliaDefault_branch = 'master' # Python 0.0%
 # RailsDefault_branch = 'main' # Python 0.0%
 # KibanaDefault_branch = 'main' # Python 0.0%
-cpythonDefault_branch = 'main' # Python 62.8% --------- X
+# cpythonDefault_branch = 'main' # Python 62.8% --------- X
 # cmsswDefault_branch = 'master' # Python 28.6% ---------
 # bootstrapDefault_branch = "main" # Python 0.0%
 # electronDefault = "main" # Python 1.9%
@@ -403,16 +403,16 @@ cpythonDefault_branch = 'main' # Python 62.8% --------- X
 # nextcloudDefault = "maser" # Python 0.0%
 # nixpkgsDefault = "master" #  Python 1.2%
 # owncloudDefault  = "maser" # Python 0.0%
-# saltDefault_branch  = "maser" # Python 97.8% --------- X
+saltDefault_branch  = "maser" # Python 97.8% --------- X
 
 # file_regex = ":*.cpp", ":*.py", ":*.c"
 
-mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Cpython_backport_keywordsPRsNoTestNeeded.csv', 
-cpythonDefault_branch,
-cpythonDictOfActiveBranches,
-'python',
-'cpython',
-'slicerOutput/Incmp_BackTrans_Cpython_backport_keywordsPRsNoNeedTestDiff.csv',
+mainCSLICER('data_cmp_incmpWithTest/Manual_incmp_Salt_backport_keywordsPRsNoTestNeeded.csv', 
+saltDefault_branch,
+saltDictOfActiveBranches,
+'saltstack',
+'salt',
+'slicerOutput/Incmp_BackTrans_Salt_backport_keywordsPRsNoNeedTestDiff.csv',
 trainingFile = 'transInput/TrainingSample.jsonl',
 testingFile =''
 )
